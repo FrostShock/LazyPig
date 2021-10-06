07c54289f5d78247e8f22f77232022b98bc6e3ee
FrostShock <46273569+FrostShock@users.noreply.github.com>
2021-10-06 13:55:23 +0200

    Add files via upload

--- LazyPig/LazyPig.lua
+++ LazyPig/LazyPig.lua
@@ -185,6 +185,50 @@ local LazyPigMenuStrings = {
 
 local CTS_MainFrame;
 
+-- Find_Herbs_Minerals START
+
+function LazyPig_Find_Herbs_Minerals()
+-- For Herbalism the icon is "INV_Misc_Flower_02"
+  if GetTrackingTexture() == "Interface\\Icons\\Spell_Nature_Earthquake" then
+    CastSpellByName("Find Herbs");
+  else
+    if PlayerHas_Find_Minerals then
+      CastSpellByName("Find Minerals");
+    else
+      CastSpellByName("Find Herbs");
+    end
+  end
+  
+-- Debug scripts
+--  PlaySound("RaidWarning");
+--  if PlayerHas_Find_Herbs then PlaySound("GnomeExploration"); end
+--  if PlayerHas_Find_Minerals then PlaySound("RaidWarning"); end
+--  MyTime = GetTime();
+--  echo(MyTime);
+--  DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Time: ");
+--  DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Time: "..MyTime);
+--  DEFAULT_CHAT_FRAME:AddMessage("|cffffff00Time: "..MyTime.." - "..tostring(PlayerHas_Find_Herbs).." - "..tostring(PlayerHas_Find_Minerals));
+end
+
+PlayerHas_Find_Herbs = false;
+PlayerHas_Find_Minerals = false;
+
+function DetectGathers()
+  for i=1,MAX_SPELL_PAGES do
+    local spellName = GetSpellName(i,"spellbook");
+    if spellName == "Find Herbs" then
+      PlayerHas_Find_Herbs = true
+      DEFAULT_CHAT_FRAME:AddMessage("|cffffff00 -- Herbalism detected");
+    end
+    if spellName == "Find Minerals" then
+      PlayerHas_Find_Minerals = true
+      DEFAULT_CHAT_FRAME:AddMessage("|cffffff00 -- Mining detected");
+    end
+  end
+end
+
+-- Find_Herbs_Minerals END
+
 -- ShaguClock START
 
 local SCframe = CreateFrame("Button", "Clock", UIParent)
@@ -276,6 +320,7 @@ function LazyPig_OnLoad()
 	this:RegisterEvent("PLAYER_LOGIN")
 	--this:RegisterEvent("PLAYER_ENTERING_WORLD")
 
+  DetectGathers();
 	ChatTimestamps_OnLoad();
 end
 
